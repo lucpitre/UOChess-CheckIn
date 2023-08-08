3b2f1ba8e02024e2875af41f117be9f086788cdb
Lucas <lucaspitre@gmail.com>
2023-08-08 18:21:54 -0400

    added tables MEMBERS and VISITS and left code for their creation

--- src/data.py
+++ src/data.py
@@ -2,3 +2,28 @@ import sqlite3
 import os.path
 
 conn = sqlite3.connect(os.path.dirname(__file__) +'/../db/members.db')
+
+
+
+def memberExists(id):
+    return False
+
+def addMember():
+    return
+
+def addVisit():
+    return
+
+
+
+### SQL commands to create tables###
+
+#conn.execute('''CREATE TABLE MEMBERS
+#         (ID INT PRIMARY KEY     NOT NULL,
+#         NAME           TEXT    NOT NULL,
+#         FIRST_YEAR     INT,
+#         ACTIVE         BIT);''')
+
+#conn.execute('''CREATE TABLE VISIT
+#         (ID            INT     NOT NULL,
+#         DAY            DATE    NOT NULL);''')
